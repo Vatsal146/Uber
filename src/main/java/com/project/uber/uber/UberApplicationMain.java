0e29493dbb39b77f922791a63f563991976dc7f3
vatsal146 <vatsal.github@gmail.com>
2024-11-16 20:36:30 +0530

    Added springboot starter files

---
+++ src/main/java/com/project/uber/uber/UberApplicationMain.java
@@ -0,0 +1,13 @@
+package com.project.uber.uber;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UberApplicationMain {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UberApplicationMain.class, args);
+	}
+
+}
